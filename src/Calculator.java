690a3bdf8f1fe91b166362a7a8bdcbe07d05c82a
JoaoPaulo <jplo@cin.ufpe.br>
2018-04-12 10:30:28 -0300

    Calculator interface rmi

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Calculator extends Remote{
+	
+	public int add(int v1, int v2) throws RemoteException;
+	public int sub(int v1, int v2) throws RemoteException;
+	public int mul(int v1, int v2) throws RemoteException;
+	public int div(int v1, int v2) throws RemoteException;
+}
